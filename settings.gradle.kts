2ebce6517955f99cda0c2410e1cfc7b6613dbb9c
Zhaolinlin <linlin.zhao@mxplayer.in>
2024-12-11 18:21:03 +0800

    fix compile error.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 //include(":app", ":libaums", ":httpserver", ":storageprovider", ":javafs", ":libusbcommunication", ":androidtests")
-include(":app", ":libaums", ":httpserver", ":storageprovider", ":javafs", ":libusbcommunication")
+include(":libusbcommunication")
